0955fd0af618434ac358cc3ce869ab4a41b08645
Mark <jungtaehoon@icloud.com>
2017-01-25 16:53:32 -0800

    Create mock data

---
+++ mockdata.js
@@ -0,0 +1,70 @@
+var list = [
+  {
+    image:  'https://static1.squarespace.com/static/547f29bfe4b0dc192ed7bdac/54aeb15ce4b018c14f34c7cb/54aeb160e4b018c14f34c7ed/1420734817363/san-franc.jpg',
+    title: 'Swimming under the Golden Gate Bridge',
+    tourGuide: 'Mark',
+    price: '$125',
+    time: '2 hours',
+    rating: 5.0,
+    numberOfReviews: 27,
+    id: 1,
+    description: 'Loemkjfadhljkadshfkljadshgkljadshgkljadhglkhadgkljhadfklghadflkjsadfdsafasdgsdgadsggh'
+  },
+  {
+    image:  'https://static1.squarespace.com/static/547f29bfe4b0dc192ed7bdac/54aeb15ce4b018c14f34c7cb/54aeb160e4b018c14f34c7ed/1420734817363/san-franc.jpg',
+    title: 'Swimming under the Golden Gate Bridge',
+    tourGuide: 'Mark',
+    price: '$125',
+    time: '2 hours',
+    rating: 3.5,
+    numberOfReviews: 43,
+    id: 1,
+    description: 'Loemkjfadhljkadshfkljadshgkljadshgkljadhglkhadgkljhadfklghadflkjsadfdsafasdgsdgadsggh'
+  },
+  {
+    image:  'https://static1.squarespace.com/static/547f29bfe4b0dc192ed7bdac/54aeb15ce4b018c14f34c7cb/54aeb160e4b018c14f34c7ed/1420734817363/san-franc.jpg',
+    title: 'Swimming under the Golden Gate Bridge',
+    tourGuide: 'Mark',
+    price: '$125',
+    time: '2 hours',
+    rating: 0.2,
+    numberOfReviews: 56,
+    id: 1,
+    description: 'Loemkjfadhljkadshfkljadshgkljadshgkljadhglkhadgkljhadfklghadflkjsadfdsafasdgsdgadsggh'
+  },
+  {
+    image:  'https://static1.squarespace.com/static/547f29bfe4b0dc192ed7bdac/54aeb15ce4b018c14f34c7cb/54aeb160e4b018c14f34c7ed/1420734817363/san-franc.jpg',
+    title: 'Swimming under the Golden Gate Bridge',
+    tourGuide: 'Mark',
+    price: '$125',
+    time: '2 hours',
+    rating: 3.1,
+    numberOfReviews: 13,
+    id: 1,
+    description: 'Loemkjfadhljkadshfkljadshgkljadshgkljadhglkhadgkljhadfklghadflkjsadfdsafasdgsdgadsggh'
+  },
+  {
+    image:  'https://static1.squarespace.com/static/547f29bfe4b0dc192ed7bdac/54aeb15ce4b018c14f34c7cb/54aeb160e4b018c14f34c7ed/1420734817363/san-franc.jpg',
+    title: 'Swimming under the Golden Gate Bridge',
+    tourGuide: 'Mark',
+    price: '$125',
+    time: '2 hours',
+    rating: 4.3,
+    numberOfReviews: 65,
+    id: 1,
+    description: 'Loemkjfadhljkadshfkljadshgkljadshgkljadhglkhadgkljhadfklghadflkjsadfdsafasdgsdgadsggh'
+  },
+  {
+    image:  'https://static1.squarespace.com/static/547f29bfe4b0dc192ed7bdac/54aeb15ce4b018c14f34c7cb/54aeb160e4b018c14f34c7ed/1420734817363/san-franc.jpg',
+    title: 'Swimming under the Golden Gate Bridge',
+    tourGuide: 'Mark',
+    price: '$125',
+    time: '2 hours',
+    rating: 2,
+    numberOfReviews: 2,
+    id: 1,
+    description: 'Loemkjfadhljkadshfkljadshgkljadshgkljadhglkhadgkljhadfklghadflkjsadfdsafasdgsdgadsggh'
+  } 
+];
+
+// var tourDetails = {}
