4c4bc0e68815c966a953680dedd7c6ef6ae1d3c2
Alison Reed <alison.michelle.reed@gmail.com>
2017-02-06 14:44:56 -0800

    Update logout

--- client/modules/Logout.js
+++ client/modules/Logout.js
@@ -20,7 +20,6 @@ export default class Logout extends React.Component {
       <div className="text-center logout">
         <h1>Are you sure you want to log out?</h1>
           <button type="button" className="btn btn-primary " onClick={this.handleLogout}>Log Out </button>
-          <button type="button" className="btn btn-primary ">Back to Home</button>
 
       </div>
     );
