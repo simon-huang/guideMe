7dfe91a529e1a8ba8e381bc588ca76920d79e11c
Aaron Min <min.aaron.min@gmail.com>
2017-02-10 12:28:18 -0800

    Go back to original database

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,4 @@
-// import controller from '../controllers';
+import controller from '../controllers';
 import path from 'path';
 //import userController from '../../db/users/userController';
 //import tourController from '../../db/tours/tourController';
@@ -19,14 +19,14 @@ var logOut = function(req, res) {
 };
 
 export default function routes(app, express) {
-  //app.get('/api/tours', controller.tours.getAll);
-  //app.get('/api/tours/:id', controller.tours.getOne);
-  //app.post('/api/tours', checkUser, controller.tours.post);
-  //
-  //app.get('/api/users/:username', controller.users.getUserForPage);
-  //app.get('/auth/logout', logOut);
-  //app.post('/auth/login', controller.users.getUserForLogin);
-  //app.post('/auth/signup', controller.users.post);
+  app.get('/api/tours', controller.tours.getAll);
+  app.get('/api/tours/:id', controller.tours.getOne);
+  app.post('/api/tours', checkUser, controller.tours.post);
+  
+  app.get('/api/users/:username', controller.users.getUserForPage);
+  app.get('/auth/logout', logOut);
+  app.post('/auth/login', controller.users.getUserForLogin);
+  app.post('/auth/signup', controller.users.post);
   
   app.use('*', (req, res, next) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
