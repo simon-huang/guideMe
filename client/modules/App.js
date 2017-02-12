beb2c561947b505a7805b645aac0bba3495e8b61
Aaron Min <min.aaron.min@gmail.com>
2017-02-12 15:07:07 -0800

    Fix ordering of navbar

--- client/modules/App.js
+++ client/modules/App.js
@@ -18,16 +18,17 @@ class App extends React.Component {
         <ul role="nav" className="nav nav-pills">
           <li className="navb"><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
           <li className="navb"><NavLink to="/AllLists">All Tours</NavLink></li>
+          <li className="navb"><NavLink to="/Create">Create Tour</NavLink></li>
+
           {
             this.props.user.username === '' ? 
-              (<li className="navb"><NavLink to="/LogIn">Log in</NavLink></li>) : 
+              (<li className="navbar-right navb"><NavLink to="/SignUp">Sign up</NavLink></li>) : ''
+          }
+          {
+            this.props.user.username === '' ? 
+              (<li className="navbar-right navb"><NavLink to="/LogIn">Log in</NavLink></li>) : 
               (<li className="navbar-right navb"><NavLink to="/Logout">Log out</NavLink></li>) 
           }
-          {
-            this.props.user.username === '' ? 
-              (<li className="navb"><NavLink to="/SignUp">Sign up</NavLink></li>) : ''
-          }
-          <li className="navbar-right navb"><NavLink to="/Create">Create Tour</NavLink></li>
         </ul>
         { 
           this.props.tours ? 
